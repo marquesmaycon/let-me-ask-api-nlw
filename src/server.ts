c6508349b40765e0e661eedfa62de2a5feb58641
Maycon Marques <mayconmarquesh@gmail.com>
2025-07-08 20:41:13 -0300

    add example .env file and implement getRooms route

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 } from 'fastify-type-provider-zod'
 
 import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -18,5 +19,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () => {
   return { status: 'ok' }
 })
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT })
