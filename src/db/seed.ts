149e5d3ceed85ec38cec6300969db19716b83c54
Maycon Marques <mayconmarquesh@gmail.com>
2025-07-08 19:58:33 -0300

    drizzle setup

---
+++ src/db/seed.ts
@@ -0,0 +1,23 @@
+import { reset, seed } from 'drizzle-seed'
+
+import { client, db } from './connection.ts'
+import { schema } from './schema/index.ts'
+
+await reset(db, schema)
+
+await seed(db, schema).refine((f) => {
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+    },
+  }
+})
+
+await client.end()
+
+// biome-ignore lint/suspicious/noConsole: only for development
+console.log('Database seeded successfully.')
